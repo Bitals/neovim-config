4af14e8a73c8e20aa5a316d6b19990bccca8dc1e
Bitals <me@bitals.xyz>
2024-07-19 00:16:35 +0400

    feat: add yanking highlight

--- init.lua
+++ init.lua
@@ -4,6 +4,18 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 -- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
